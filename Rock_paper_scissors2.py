1a5c40b64bee57b545734ff184957c9825a6a4c0
zakmich <WYZv7XbX@Jwuktb>
2023-03-14 22:54:56 +0100

    Refactored points returning system

--- Rock_paper_scissors2.py
+++ Rock_paper_scissors2.py
@@ -9,18 +9,18 @@ def choice_input(player):
         if player_choice in valid_options:
             return player_choice
 def whow_win(player_one_choice, player_two_choice):
-    if player_one_choice == "rock" and player_two_choice == "scissors" \
-    or player_one_choice == "paper" and player_two_choice == "rock" \
-    or player_one_choice == "scissors" and player_two_choice == "paper":
-        print("Player one won")
-        return 1
-
-    elif player_one_choice == player_two_choice:
+    if player_one_choice == player_two_choice:
         print("Draw")
         return 0
-    else:
-        print("Player two won")
-        return 2
+
+    possibilities = {
+        ("rock", "scissors"): 1,
+        ("paper", "rock"): 1,
+        ("scissors", "paper"): 1
+        }
+
+    return possibilities.get((player_one_choice, player_two_choice), 2)
+
 
 while p1_score != 3 and p2_score != 3:
     print("Chose rock, paper or scissors")
@@ -29,8 +29,10 @@ while p1_score != 3 and p2_score != 3:
     score = whow_win(player_one_choice, player_two_choice)
 
     if score == 1:
+        print("Player one won")
         p1_score += 1
     elif score == 2:
+        print("Player two won")
         p2_score += 1
 
     print("Player 1 score:", p1_score,"|" " Player 2 score:", p2_score)
